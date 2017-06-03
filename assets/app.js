b4fa4d346ec109e34c02eca8442ae72a15451b0d
Brandon Barulich <BrandonBarulich@Brandons-MacBook-Pro.local>
2017-06-02 22:42:30 -0500

    final3

--- assets/app.js
+++ assets/app.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     $(".sports-button").removeClass("active");
     $(this).addClass("active");
     var type = $(this).attr("data-type");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
       url: queryURL,
       method: "GET"
